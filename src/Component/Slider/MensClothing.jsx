4760a83e258c7ecd2bba992f9f3b3312d83e8669
Mohamed Elbana <88998148+Mo7amed-2lbana@users.noreply.github.com>
2023-05-10 17:11:06 +0300

    Update MensClothing.jsx

--- src/Component/Slider/MensClothing.jsx
+++ src/Component/Slider/MensClothing.jsx
@@ -20,6 +20,32 @@ export default function MensSlider() {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <>
